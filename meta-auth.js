195c4bb212c30ac3aaf3154c0037ccfa87af5e38
Leon Acosta <leon@dandelionlabs.io>
2021-12-27 23:53:44 +0700

    Improved messaging

--- meta-auth.js
+++ meta-auth.js
@@ -62,12 +62,12 @@ class MetaAuth {
 
         cache.set(address, hash);
 
-        return `You are signing a challenge to enter in OxyFarms' whitelist: "${hash}"`;
+        return `You are signing a challenge to enter in OxyFarms' whitelist: ${hash}`;
     }
 
     checkChallenge(challenge, sig) {
         const recovered = sigUtil.recoverPersonalSignature({
-            data: `You are signing a challenge to enter in OxyFarms' whitelist: "${hash}"`,
+            data: `You are signing a challenge to enter in OxyFarms' whitelist: ${hash}`,
             sig
         });
 
